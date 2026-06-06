901e241606cd3039fa54562a8df3d2894cc70510
Lurbik <ludivico.toto@gmail.com>
2026-06-06 15:39:41 +0200

    creazione iniziale

---
+++ src/main/java/it/uniroma3/siw/Artie5ive/Artie5iveApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.Artie5ive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Artie5iveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Artie5iveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/Artie5ive/Artie5iveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.Artie5ive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artie5iveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
