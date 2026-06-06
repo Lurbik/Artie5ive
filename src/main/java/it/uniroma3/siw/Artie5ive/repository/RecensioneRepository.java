1a2b21830e75d956383f6f1bd2dfbc6eef254bec
anastasiaswag <ipersam04@gmail.com>
2026-06-06 16:55:59 +0200

    cartelle fatte
    repository ci sono utente e recensione, utenteservice è vuota

---
+++ src/main/java/it/uniroma3/siw/Artie5ive/repository/RecensioneRepository.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.Artie5ive.repository;
+
+import it.uniroma3.siw.Artie5ive.model.Recensione;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface RecensioneRepository extends JpaRepository<Recensione, Long> {
+    List<Recensione> findByVinoId(Long vinoId);
+    List<Recensione> findByUtenteId(Long utenteId);
+    Optional<Recensione> findByUtenteIdAndVinoId(Long utenteId, Long vinoId);
+    boolean existsByUtenteIdAndVinoId(Long utenteId, Long vinoId);
+}
